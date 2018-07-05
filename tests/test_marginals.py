8cad67ff19cb4d6628f5ac0827f16eaf966c9102
rbardenet <remi.bardenet@gmail.com>
2018-07-05 18:18:31 +0200

    added a test for doubletons

--- tests/test_marginals.py
+++ tests/test_marginals.py
@@ -39,6 +39,45 @@ class FollowsTheRightMarginal(unittest.TestCase):
         chi2, pval = sps.chisquare(observedFrequencies, expectedFrequencies)
         self.failUnless(pval>0.05)
 
+# Here's our "unit tests".
+class FollowsTheRightMarginalForDoubletons(unittest.TestCase):
+
+    def testGS(self):
+        """
+        test whether GS samples the right marginals
+        """
+        npr.seed(1)
+
+        # build a projection matrix K
+        N = 10
+        A = npr.randn(N**2).reshape((N,N))
+        Q, R = npl.qr(A)
+        L = np.eye(N)
+        r = N//2 # rank of the projection used for testing
+        for i in range(N-r):
+            L[i,i] = 0
+        K = Q.dot(L.dot(Q.T)) # projection matrix by construction
+        dpp = Discrete_DPP(kernel_type="inclusion", proj=True, K=K)
+
+        # sample from the dpp
+        numSamples = 100
+        for _ in range(numSamples):
+            dpp.sample_exact(sampling_mode="GS")
+
+        # Sample doubletons with high likelihood and count them
+        doubletons = [npr.choice(N, size=2, p=np.diag(K)/r, replace=False)
+                for _ in range(10)]
+        counts = []
+        for doubleton in doubletons:
+            counts.append(np.sum([set(doubleton).issubset(set(sample))
+                for sample in dpp.list_of_samples]))
+        observedFrequencies = np.array(counts)/len(dpp.list_of_samples)
+
+        # perform chi-square test
+        expectedFrequencies = [npl.det(K[d,:][:,d]) for d in doubletons]
+        chi2, pval = sps.chisquare(observedFrequencies, expectedFrequencies)
+        self.failUnless(pval>0.05)
+
 def main():
     unittest.main()
 
