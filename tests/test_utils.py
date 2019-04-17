00372738cf749e45018357752002a90489f072f0
Guillaume Gautier <guillaume.gautier@univ-lille1.fr>
2019-04-17 11:47:16 +0200

    add few more tests on utility functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,25 +12,35 @@ sys.path.append('..')
 
 from numpy import allclose
 from numpy.random import rand
-from dppy.utils import inner1d
+from dppy.utils import inner1d, det_ST
 
 
-class TestInner1d(unittest.TestCase):
+class TestUtils(unittest.TestCase):
     """ Test
     """
 
-    X = rand(10, 20, 30, 40)
-    Y = rand(*X.shape)
-
     def test_inner_products_and_square_norms(self):
 
-        for ax in range(self.X.ndim):
+        X = rand(10, 20, 30, 40)
+        Y = rand(*X.shape)
 
-            self.assertTrue(allclose(inner1d(self.X, self.Y, axis=ax),
-                                     (self.X * self.Y).sum(axis=ax)))
-            self.assertTrue(allclose(inner1d(self.X, axis=ax),
-                                     (self.X**2).sum(axis=ax)))
+        for ax in range(X.ndim):
 
+            self.assertTrue(allclose(inner1d(X, Y, axis=ax),
+                                     (X * Y).sum(axis=ax)))
+            self.assertTrue(allclose(inner1d(X, axis=ax),
+                                     (X**2).sum(axis=ax)))
+
+    def test_det_ST(self):
+        """ Compute :math:`\det M_{S, T} = \det [M_{ij}]_{i\inS, j\in T}`
+        """
+
+
+        if T is None:  # det M_SS = det M_S
+            return det(array[np.ix_(S, S)])
+
+        else:  # det M_ST, numpy deals with det M_[][] = 1.0
+            return det(array[np.ix_(S, T)])
 
 def main():
 
