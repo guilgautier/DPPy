8d4f372947c898af4539e0588d15192861874afb
daniele <daniele.calandriello@iit.it>
2020-05-17 13:44:59 +0200

    Added bless_size algorithm, cleaned up a bit bless file.

--- dppy/bless.py
+++ dppy/bless.py
@@ -26,6 +26,7 @@
 
 import numpy as np
 from collections import namedtuple
+
 from .utils import check_random_state, stable_invert_root, get_progress_bar, evaluate_L_diagonal
 
 CentersDictionary = namedtuple('CentersDictionary', ('idx', 'X', 'probs', 'lam', 'rls_oversample'))
@@ -66,7 +67,7 @@ def estimate_rls_bless(D, X, eval_L, lam_new):
     # ell-2 norm of the columns of (X'X + lam*S^(-2))^(-1/2)XX'
     rls_estimate = (diag_norm - np.square(X_precond, out=X_precond).sum(axis=0)) / lam_new
 
-    if not np.all(rls_estimate  >= 0.):
+    if not np.all(rls_estimate >= 0.):
         raise ValueError('Some estimated RLS is negative, this should never happen.'
                          ' Min prob: {}'.format(np.min(rls_estimate)))
 
@@ -80,8 +81,8 @@ def reduce_lambda(X_data, eval_L, intermediate_dict_bless, lam_new, rng, rls_ove
     :param callable eval_L: likelihood function
     :param CentersDictionary intermediate_dict_bless: an (eps, lambda) accurate dictionary, see :ref:`bless`
     :param float lam_new: lambda regularization for the new dictionary
-    :param RandomState rng: rng for sampling
-    :param rls_oversample_parameter: Oversampling parameter to increase success probability, see :ref:`bless`
+    :param np.random.RandomState rng: rng for sampling
+    :param float rls_oversample_parameter: Oversampling parameter to increase success probability, see :ref:`bless`
     :return: An (eps, lam_new)-accurate dictionary with high probability
     :rtype:
         CentersDictionary
@@ -171,7 +172,7 @@ def bless(X_data, eval_L, lam_final, rls_oversample_param, random_state=None, nb
         Roughly, the final dictionary will approximate all principal components with a singular value
         larger than lam_final, and therefore smaller lam_final creates larger, more accurate dictionaries.
 
-    :param int rls_oversample_param: Oversampling parameter used during BLESS's step of random RLS sampling.
+    :param float rls_oversample_param: Oversampling parameter used during BLESS's step of random RLS sampling.
         The rls_oversample >= 1 parameter is used to increase the sampling probabilities and sample size by a
         rls_oversample factor. This linearly increases the size of the output dictionary, making the algorithm
         less memory and time efficient, but reduces variance and the negative effects of randomness on the accuracy
@@ -224,6 +225,7 @@ def bless(X_data, eval_L, lam_final, rls_oversample_param, random_state=None, nb
 
     # force at least one sample to be selected
     selected_init[0] = 1
+    ucb_init[0] = rls_oversample_param * 1.
 
     intermediate_dict_bless = CentersDictionary(idx=selected_init.nonzero(),
                                                 X=X_data[selected_init, :],
@@ -249,3 +251,132 @@ def bless(X_data, eval_L, lam_final, rls_oversample_param, random_state=None, nb
             t.update()
 
     return intermediate_dict_bless
+
+
+def bless_size(X_data, eval_L, size_final, rls_oversample_param, random_state=None, nb_iter_bless=None, verbose=True):
+
+    """Returns a (eps, lam_max)-accurate dictionary of Nystrom centers sampled according to approximate RLS.
+    This variant automatically chooses the lambda parameter, and returns a [lam_min, lam_max] interval
+    such that w.h.p. deff(lam_min) <= size_final/2 and 2*size_final <= deff(lam_max).
+
+    Given data X, a similarity function, and its related similarity matrix similarity_function(X, X),
+    an (eps, lam_max)-accurate dictionary approximates all principal components of the similarity matrix
+    with a singular value larger than lam_max, up to a (1+eps) multiplicative error.
+
+    The algorithm is introduced and analyzed in :cite:`CaDeVa20` in the context of k-DPP sampling,
+    for a more formal definition of (eps, lambda)-accuracy and other potential uses see :cite:`CaLaVa17`.
+
+    :param array_like X_data: input data, as an ndarray-like (n x m) object
+
+    :param callable eval_L: likelihood function between points.
+        If L is the associated likelihood matrix, it must satisfy the interface
+        eval_L(X_1) = K(X_1, X_1)
+        eval_L(X_1, X_2) = K(X_1, X_2)
+        This interface is inspired by scikit-learn's implementation of kernel functions in Gaussian Processes.
+        Any of the kernels provided by sklearn (e.g. sklearn.gaussian_process.kernels.RBF or
+        sklearn.gaussian_process.kernels.PairwiseKernel) should work out of the box.
+
+    :param int size_final: desired final size. The algorithm returns an interval [lam_min, lam_max]
+        that can be used to sample from a "size_final"-DPP. Therefore, larger
+        size_final usually creates larger, more accurate dictionaries.
+
+    :param int rls_oversample_param: Oversampling parameter used during BLESS's step of random RLS sampling.
+        The rls_oversample >= 1 parameter is used to increase the sampling probabilities and sample size by a
+        rls_oversample factor. This linearly increases the size of the output dictionary, making the algorithm
+        less memory and time efficient, but reduces variance and the negative effects of randomness on the accuracy
+        of the algorithm. Empirically, a small factor rls_oversample = [2,10] seems to work.
+        It is suggested to start with a small number and increase if the algorithm fails to terminate or is inaccurate.
+
+    :param random_state: Random number generator (RNG) used for the algorithm.
+        By default, if random_state is not provided or is None, a numpy's RandomState with default seeding is used.
+        If a numpy's RandomState is passed, it is used as RNG. If an int is passed, it is used to seed a RandomState
+    :type random_state:
+        np.random.RandomState or int or None, default None
+
+
+    :param nb_iter_bless: number of iterations, defaults to log_2((1 + 4/(size_final - 2)*tr(L))) if None,
+    which guarantees a reduction rate of 2 for lambda.
+    :type nb_iter_bless:
+        int or None, default None
+
+    :param bool verbose: Controls verbosity of debug output, including progress bars.
+        The progress bar reports:
+        - lam: lambda value of the current iteration
+        - m: current size of the dictionary (number of centers contained)
+        - m_expected: expected size of the dictionary before sampling
+        - probs_dist: (mean, max, min) of the approximate RLSs at the current iteration
+
+    :return:  A tuple containing an interval [lam_min, lam_max] that can be used to sample
+        from a "size_final"-DPP, and an (eps, lambda)-accurate dictionary centers_dict
+        (with high probability). If centers_dict contains m entries then the output fields are as follow
+
+        centers_dict.idx`: the indices of the m selected samples in the input dataset `X`
+        centers_dict.X': the (m x d) numpy.ndarray containing the selected samples
+        centers_dict.probs: the probabilities (i.e. approximate RLSs) used to sample the dictionary
+        lam: the final lambda accuracy
+        rls_oversample: the rls_oversample used to sample the dictionary, as a proxy for the `eps`-accuracy
+
+    :rtype:
+        tuple(list, CentersDictionary)
+    """
+
+    n, d = X_data.shape
+
+    diag_norm = np.asarray(evaluate_L_diagonal(eval_L, X_data))
+
+    lam_init = diag_norm.sum()/(size_final - 1.)
+    lam_max = lam_init
+
+    lam_final = 1
+    lam_min = lam_final
+
+    if nb_iter_bless is None:
+        nb_iter_bless = np.ceil(np.log2(lam_init)).astype('int')
+    lam_sequence = list(np.geomspace(lam_final, lam_init, nb_iter_bless))
+
+    ucb_init = rls_oversample_param * diag_norm / (diag_norm + lam_init)
+
+    rng = check_random_state(random_state)
+
+    selected_init = rng.rand(n) <= ucb_init
+
+    # force at least one sample to be selected
+    selected_init[0] = 1
+    ucb_init[0] = rls_oversample_param * 1.
+
+    intermediate_dict_bless = CentersDictionary(idx=selected_init.nonzero(),
+                                                X=X_data[selected_init, :],
+                                                probs=np.ones(np.sum(selected_init)) * ucb_init[selected_init],
+                                                lam=lam_init,
+                                                rls_oversample=rls_oversample_param)
+
+    # discard lam_init from the list, we already used it to initialize
+    lam_new = lam_sequence.pop()
+    deff_hat_new = size_final - 1
+
+    with get_progress_bar(total=len(lam_sequence), disable=not verbose) as t:
+        while len(lam_sequence) > 0:
+            lam_old = lam_new
+            deff_hat_old = deff_hat_new
+
+            lam_new = lam_sequence.pop()
+            intermediate_dict_bless = reduce_lambda(X_data, eval_L, intermediate_dict_bless, lam_new, rng)
+            deff_hat_new = len(intermediate_dict_bless.idx)/intermediate_dict_bless.rls_oversample
+
+            # check if the dictionary passed the lower threshold, in which case set it to last valid lambda
+            if deff_hat_old <= (size_final - 1)/2 and deff_hat_new >= (size_final - 1)/2:
+                lam_max = lam_old
+
+            if deff_hat_new >= 2 * (size_final + 1):
+                lam_min = lam_new
+                break
+
+            t.set_postfix(lam=int(lam_new),
+                          m=len(intermediate_dict_bless.probs),
+                          m_expected=int(intermediate_dict_bless.probs.mean() * n),
+                          probs_dist="({:.4}, {:.4}, {:.4})".format(intermediate_dict_bless.probs.mean(),
+                                                                    intermediate_dict_bless.probs.max(),
+                                                                    intermediate_dict_bless.probs.min()))
+            t.update()
+
+    return (lam_max, lam_min), intermediate_dict_bless
