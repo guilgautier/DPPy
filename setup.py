37ca8e39f5adfe95e169f5f2cdf4bc24a0203cdf
Guillaume Gautier <guillaume.gga@gmail.com>
2020-10-23 10:10:20 +0200

    Create proper version 0.3.1 reducing dependencies on installation

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.1b1',
+    version='0.3.1',
 
     description='DPPy is a Python library for exact and approximate sampling of Determinantal Point Processes.',
     long_description=long_description,
