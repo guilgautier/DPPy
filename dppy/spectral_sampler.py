b84be92f3658d1477d92d08d9bc9ccea65290f25
Guillaume Gautier <guillaume.gga@gmail.com>
2021-04-15 13:46:34 +0200

    Continue refactoring spectral_sampler

--- dppy/spectral_sampler.py
+++ dppy/spectral_sampler.py
@@ -1,5 +1,7 @@
 import numpy as np
 import numpy.linalg as la
+
+from scipy.linalg import qr
 from warnings import warn
 
 from .exact_sampling import (
@@ -8,6 +10,12 @@ from .utils import (check_in_01, check_geq_0)
 
 
 def spectral_sampler(dpp, rng, **params):
+
+    compute_spectral_sampler_parameters(dpp)
+    return do_spectral_sampler(dpp, rng)
+
+
+def do_spectral_sampler(dpp, rng):
 
     if dpp.K_eig_vals is not None:
         # Phase 1
@@ -20,21 +28,36 @@ def spectral_sampler(dpp, rng, **params):
         return proj_dpp_sampler_eig(V, dpp.sampling_mode,
                                     random_state=rng)
 
-    # elif dpp.L_dual_eig_vals is not None:
-    #     # Phase 1
-    #     V = dpp_eig_vecs_selector_L_dual(dpp.L_dual_eig_vals,
-    #                                      dpp.L_dual_eig_vecs,
-    #                                      dpp.L_gram_factor,
-    #                                      random_state=rng)
-    #     # Phase 2
-    #     return proj_dpp_sampler_eig(V, dpp.sampling_mode,
-    #                                  random_state=rng)
-    #
+    # # If DPP defined via projection correlation kernel K
+    # # no eigendecomposition required
+    elif dpp.K is not None and dpp.projection:
+        return proj_dpp_sampler_kernel(dpp.K, dpp.sampling_mode,
+                                       random_state=rng)
+    else:
+        raise ValueError('None of the available samplers could be used based on the current DPP representation. This should never happen, please consider rasing an issue on github at https://github.com/guilgautier/DPPy/issues')
 
-    elif dpp.L_eig_vals is not None:
+
+def compute_spectral_sampler_parameters(dpp):
+    """Compute eigenvalues and eigenvectors of correlation kernel K from various parametrizations of ``dpp``
+
+    :param dpp: ``FiniteDPP`` object
+    :type dpp: ``FiniteDPP`` object
+    """
+    while compute_spectral_sampler_parameters_step(dpp):
+        pass
+
+
+def compute_spectral_sampler_parameters_step(dpp):
+    """
+    Returns
+    ``False`` if the right parameters are indeed computed
+    ``True`` if extra computations are required
+
+    Note: Sort of fixed point algorithm to find dpp.K_eig_vals and dpp.eig_vecs
+    """
+    if dpp.L_eig_vals is not None:
         dpp.K_eig_vals = dpp.L_eig_vals / (1.0 + dpp.L_eig_vals)
-        return dpp.sample_exact(mode=dpp.sampling_mode,
-                                random_state=rng)
+        return False
 
     elif dpp.L_dual is not None:
         # L_dual = Phi * Phi.T = W Theta W.T
@@ -45,26 +68,17 @@ def spectral_sampler(dpp, rng, **params):
         dpp.L_eig_vals = np.maximum(dpp.L_eig_vals, 0.0)
         dpp.eig_vecs = dpp.L_gram_factor.T.dot(L_dual_eig_vecs
                                                / np.sqrt(dpp.L_eig_vals))
-        return dpp.sample_exact(mode=dpp.sampling_mode,
-                                random_state=rng)
-
-    # If DPP defined via projection correlation kernel K
-    # no eigendecomposition required
-    elif dpp.K is not None and dpp.projection:
-        return proj_dpp_sampler_kernel(dpp.K, dpp.sampling_mode,
-                                       random_state=rng)
+        return True
 
     elif dpp.K is not None:
         dpp.K_eig_vals, dpp.eig_vecs = la.eigh(dpp.K)
         check_in_01(dpp.K_eig_vals)
-        return dpp.sample_exact(mode=dpp.sampling_mode,
-                                random_state=rng)
+        return False
 
     elif dpp.L is not None:
         dpp.L_eig_vals, dpp.eig_vecs = la.eigh(dpp.L)
         check_geq_0(dpp.L_eig_vals)
-        return dpp.sample_exact(mode=dpp.sampling_mode,
-                                random_state=rng)
+        return True
 
     # If DPP defined through correlation kernel with parameter 'A_zono'
     # a priori you wish to use the zonotope approximate sampler
@@ -72,14 +86,12 @@ def spectral_sampler(dpp, rng, **params):
         warn('DPP defined via `A_zono`, apriori you want to use `sample_mcmc`, but you have called `sample_exact`')
 
         dpp.K_eig_vals = np.ones(dpp.A_zono.shape[0])
-        dpp.eig_vecs, _ = la.qr(dpp.A_zono.T, mode='economic')
-        return dpp.sample_exact(mode=dpp.sampling_mode,
-                                random_state=rng)
+        dpp.eig_vecs, _ = qr(dpp.A_zono.T, mode='economic')
+        return False
 
     elif dpp.eval_L is not None and dpp.X_data is not None:
         dpp.compute_L()
-        return dpp.sample_exact(mode=dpp.sampling_mode,
-                                random_state=rng)
+        return True
 
     else:
         raise ValueError('None of the available samplers could be used based on the current DPP representation. This should never happen, please consider rasing an issue on github at https://github.com/guilgautier/DPPy/issues')
