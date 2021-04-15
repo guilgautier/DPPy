4de6cab7eb7709cd29043fdb15262290667fbd3c
Guillaume Gautier <guillaume.gga@gmail.com>
2021-04-15 11:05:34 +0200

    test valid eigenvalues in 01 for correlation kernel

--- tests/exact_samplers/test_spectal_sampler.py
+++ tests/exact_samplers/test_spectal_sampler.py
@@ -28,6 +28,53 @@ class TestSpectralSampler(unittest.TestCase):
         self.assertTrue(np.allclose(
             dpp.K_eig_vals, eig_vals / (1.0 + eig_vals)))
 
+    def test_correlation_eig_vals_not_in_01_from_K_raises_error(self):
+        eig_vecs, _ = qr(randn(self.N, self.r), mode="economic")
+        eig_vals = np.arange(1, self.r+1)
+
+        K = (eig_vecs * eig_vals).dot(eig_vecs.T)
+
+        dpp = FiniteDPP("correlation", False, K=K)
+
+        with self.assertRaises(ValueError):
+            sample = dpp.sample_exact(mode="GS")
+
+    def test_correlation_eig_vals_in_01_from_K(self):
+        eig_vecs, _ = qr(randn(self.N, self.N), mode="economic")
+        eig_vals = np.sort(rand(self.N))
+
+        K = (eig_vecs * eig_vals).dot(eig_vecs.T)
+
+        dpp = FiniteDPP("correlation", False, K=K)
+
+        sample = dpp.sample_exact(mode="GS")
+
+        self.assertTrue(np.allclose(dpp.K_eig_vals, eig_vals))
+
+    # def test_likelihood_dual_eig_vals_to_likelihood_eig_vals(self):
+    #     phi = randn(self.r, self.N)
+    #     L_dual = phi.dot(phi.T)
+
+    #     dpp = FiniteDPP("likelihood", False,
+    #                     L_dual=L_dual)
+
+    #     sample = dpp.sample_exact(mode="GS")
+
+    #     self.assertTrue(np.allclose(
+    #         dpp.K_eig_vals, eig_vals / (1.0 + eig_vals)))
+
+    # def test_likelihood_dual_eig_vals_to_correlation_eig_vals(self):
+    #     eig_vecs, _ = qr(randn(self.N, self.r), mode="economic")
+    #     eig_vals = np.arange(1, self.r+1)
+
+    #     dpp = FiniteDPP("likelihood", False,
+    #                     L_eig_dec=(eig_vals, eig_vecs))
+
+    #     sample = dpp.sample_exact(mode="GS")
+
+    #     self.assertTrue(np.allclose(
+    #         dpp.K_eig_vals, eig_vals / (1.0 + eig_vals)))
+
 
 def main():
 
