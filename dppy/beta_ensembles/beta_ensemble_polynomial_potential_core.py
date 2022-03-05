6658baf17f1192de957b6cf93ddd476e2d937664
Guillaume Gautier <guillaume.gga@gmail.com>
2022-03-05 08:50:06 +0100

    tests: shape tracy window testing with pytest

--- dppy/beta_ensembles/beta_ensemble_polynomial_potential_core.py
+++ dppy/beta_ensembles/beta_ensemble_polynomial_potential_core.py
@@ -774,7 +774,6 @@ class TracyWidom(object):
             # Compute K[x, y]_{x, y \in X}
             # K_xy = (A_x * dA_y - dA_x * A_y) / (x - y)
             X = np.ravel(X)
-            X = len(X)
 
             x = phi(X)
             sqrt_dphi_x = np.sqrt(d_phi(X))
@@ -797,7 +796,7 @@ class TracyWidom(object):
             # set y = x - eps
             # K_xx = (A_x * dA_x-eps - dA_x * A_x-eps) / eps
             A_y, dA_y, _, _ = airy(x - eps_xy)
-            diag = range(X)
+            diag = range(len(X))
             K_xy[:, diag, diag] = (A_x * dA_y - dA_x * A_y) / eps_xy
 
             return sqrt_dphi_x[:, None] * K_xy * sqrt_dphi_x
--- tests/beta_ensembles/test_tracy_widom.py
+++ tests/beta_ensembles/test_tracy_widom.py
@@ -1,102 +1,72 @@
-# coding: utf8
-
-import unittest
+"""Tests of the TracyWidow class based on the work of Bornemann 2010 `https://arxiv.org/pdf/0804.2543.pdf <https://arxiv.org/pdf/0804.2543.pdf>`_"""
 
 import numpy as np
+import pytest
 
 from dppy.beta_ensembles.beta_ensemble_polynomial_potential_core import TracyWidom
 
-
-class TestTracyWidom(unittest.TestCase):
-    """Based on the work of Bornemann 2010 `https://arxiv.org/pdf/0804.2543.pdf <https://arxiv.org/pdf/0804.2543.pdf>`_"""
-
-    TW = TracyWidom()
-
-    def test_kernel_example_bornemann_fredholm_determinant_should_equal_sin1(self):
-        """Equation 5.8 Bornemann"""
-
-        def K_Green(x, y):
-            Y, X = np.meshgrid(x, y)
-            return np.where(X <= Y, X * (1 - Y), Y * (1 - X))
-
-        quad_order = 50
-        x_quad, w_quad = self.TW.compute_quadrature(quad_order)
-        fred_det_K_approx = self.TW.fredholm_determinant(K_Green, x_quad, w_quad)
-        fred_det_K_theo = np.sin(1)
-
-        self.assertAlmostEqual(
-            fred_det_K_approx,
-            fred_det_K_theo,
-            msg=(fred_det_K_approx, fred_det_K_theo),
-            delta=1e-5,
-        )
-
-    def test_change_of_variables_from_0_1_to_s_oo_should_be_increasing(self):
-        """
-        .. todo::
-
-            Add refer to increasing choice
-        """
-        points = np.linspace(0, 1, 10)
-        s = -1
-        phi, d_phi = self.TW.change_of_variable(s)
-
-        for x, y in zip(points[:-1], points[1:]):
-            with self.subTest(x=x, y=y):
-                self.assertLessEqual(phi(x), phi(y))
-
-    def test_change_of_variables_from_0_1_to_s_oo_derivative_is_correct(self):
-
-        points = np.linspace(0, 1, 10, endpoint=False)
-        s = -1
-        phi, d_phi = self.TW.change_of_variable(s)
-
-        eps = 1e-7
-
-        for x in points:
-            with self.subTest(x=x):
-                d_phi_x_approx = (phi(x + eps) - phi(x)) / eps
-                d_phi_x = d_phi(x)
-
-                self.assertAlmostEqual(
-                    d_phi_x_approx,
-                    d_phi_x,
-                    msg=(x, d_phi_x_approx, d_phi_x),
-                    delta=1e-2,
-                )
-
-    def test_evaluation_Tracy_Widom_cdf(self):
-        """evalution points obtained from Table 5. in *LARGEST EIGENVALUES AND SAMPLE COVARIANCE MATRICES*, ANDREI IU. BEJAN
-        https://pdfs.semanticscholar.org/ca19/3484415f374d8fb02e7fbdad72b99727b41f.pdf?_ga=2.251544262.1964171041.1570206947-237360766.1567514713
-        """
-        points = np.array(
-            [
-                [-3.0, 0.080361],
-                [-2.5, 0.212392],
-                [-2.0, 0.413256],
-                [-1.5, 0.631401],
-                [-1.0, 0.807225],
-                [-0.5, 0.916070],
-                [0.0, 0.969375],
-                [0.5, 0.990545],
-                [1.0, 0.997506],
-                [1.5, 0.999432],
-                [2.0, 0.999888],
-            ]
-        )
-
-        quad_order = 50
-
-        tol = 1e-4
-
-        cdf_s_approx = self.TW.cdf(points[:, 0], quad_order)
-        self.assertTrue(np.allclose(cdf_s_approx, points[:, 1], atol=tol))
+TW = TracyWidom()
 
 
-def main():
+def test_kernel_example_bornemann_fredholm_determinant_should_equal_sin1():
+    """Equation 5.8 Bornemann"""
 
-    unittest.main()
+    def K_Green(x, y):
+        Y, X = np.meshgrid(x, y)
+        return np.where(X <= Y, X * (1 - Y), Y * (1 - X))
+
+    quad_order = 50
+    x_quad, w_quad = TW.compute_quadrature(quad_order)
+    approx = TW.fredholm_determinant(K_Green, x_quad, w_quad)
+    expected = np.sin(1)
+
+    np.testing.assert_almost_equal(approx, expected, decimal=5)
 
 
-if __name__ == "__main__":
-    main()
+@pytest.mark.parametrize(
+    "x, y",
+    [
+        (np.linspace(0, 1, 10)[:-1], np.linspace(0, 1, 10)[1:]),
+    ],
+)
+def test_change_of_variables_from_0_1_to_s_oo_should_be_increasing(x, y):
+    s = -1
+    phi, *_ = TW.change_of_variable(s)
+    np.testing.assert_array_less(phi(x), phi(y))
+
+
+@pytest.mark.parametrize("x", np.linspace(0, 1, 10, endpoint=False))
+def test_change_of_variables_from_0_1_to_s_oo_derivative_is_correct(x):
+    s = -1
+    phi, d_phi = TW.change_of_variable(s)
+
+    eps = 1e-7
+    approx = (phi(x + eps) - phi(x)) / eps
+    computed = d_phi(x)
+
+    np.testing.assert_almost_equal(approx, computed, decimal=2)
+
+
+@pytest.mark.parametrize(
+    "x, cdf_x",
+    [
+        [-3.0, 0.080361],
+        [-2.5, 0.212392],
+        [-2.0, 0.413256],
+        [-1.5, 0.631401],
+        [-1.0, 0.807225],
+        [-0.5, 0.916070],
+        [0.0, 0.969375],
+        [0.5, 0.990545],
+        [1.0, 0.997506],
+        [1.5, 0.999432],
+        [2.0, 0.999888],
+    ],
+)
+def test_evaluation_Tracy_Widom_cdf(x, cdf_x):
+    """evalution points obtained from Table 5. in *LARGEST EIGENVALUES AND SAMPLE COVARIANCE MATRICES*, ANDREI IU. BEJAN
+    https://pdfs.semanticscholar.org/ca19/3484415f374d8fb02e7fbdad72b99727b41f.pdf?_ga=2.251544262.1964171041.1570206947-237360766.1567514713
+    """
+    quad_order = 50
+    cdf_x_computed = TW.cdf(x, quad_order)
+    np.testing.assert_almost_equal(cdf_x_computed, cdf_x, decimal=4)
